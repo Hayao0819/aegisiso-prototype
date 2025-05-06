169f9751d8a6fb794ec9587be25d6256f40b16d5
hayao <shun819.mail@gmail.com>
2025-05-06 15:41:35 +0900

    Add xorriso arguments

--- src/image.rs
+++ src/image.rs
@@ -1,20 +1,49 @@
 use crate::error::ArchisoError;
-use std::path::Path;
 use crate::utils::run_command;
+use std::path::Path;
 
 /// Create SquashFS image from root directory
 pub async fn squash(root: &Path, out: &Path) -> Result<(), ArchisoError> {
-    run_command("mksquashfs", &[root.to_str().unwrap(), out.to_str().unwrap(), "-noappend", "-comp", "xz"]).await?;
+    run_command(
+        "mksquashfs",
+        &[
+            root.to_str().unwrap(),
+            out.to_str().unwrap(),
+            "-noappend",
+            "-comp",
+            "xz",
+        ],
+    )
+    .await?;
     Ok(())
 }
 
 /// Create ISO from work directory
 pub async fn make_iso(src: &Path, iso: &Path, volid: &str) -> Result<(), ArchisoError> {
-    run_command("xorriso", &[
-        "-as", "mkisofs", "-o",
-        iso.to_str().unwrap(),
-        "-J", "-R", "-V", volid,
-        src.to_str().unwrap()
-    ]).await?;
+    run_command(
+        "xorriso",
+        &[
+            // Emulate mkisofs
+            "-as",
+            "mkisofs",
+            // Generate Joliet directory information
+            "-joliet",
+            "-joliet-long",
+            // Allow full 31 character filenames for ISO9660 names
+            "-full-iso9660-filenames",
+            // Output file
+            "-o",
+            iso.to_str().unwrap(),
+            // Label
+            "-volid",
+            volid,
+            // Generate rationalized Rock Ridge directory information
+            "-rational-rock",
+            "-V",
+             // Source
+            src.to_str().unwrap(),
+        ],
+    )
+    .await?;
     Ok(())
 }
